187c2e4c94c96f41d69ca4a65a93ab9a7651d6bc
Parul <parulmahajands@gmail.com>
2021-09-23 22:32:52 +0530

    Initial Commit

--- game(rps).py
+++ game(rps).py
@@ -1,76 +1,80 @@
 # Rock, Paper, Scissors
 import random
 
-list1 = ("r", "p", "s")
+game_list = ("r", "p", "s")
 
 Total_Chances = 10
 No_Of_Chances = 0
 Computer_Points = 0
 Human_Points = 0
+print("\nHello, this game is developed by Codecumber.\n")
 print(" \t \t \t \t Rock, Paper, Scissors Game \n \n")
 print("r for rock \np for paper\ns for scissors \n")
 
 while No_Of_Chances < Total_Chances:
-    _input = input("Rock, Paper, Scissors: ")
-    _random = random.choice(list1)
-    if _input == _random:
-        print("Tie Both 0 point to each \n")
+      game= input("Rock, Paper, Scissors: ")
+      _random = random.choice(game_list)
 
-    # If user enters r
-    elif _input == "r" and _random == "p":
-        Computer_Points = Computer_Points + 1
-        print(f"your guess is {_input} and computer guess is {_random} \n")
-        print("Computer wins 1 point \n")
-        print(f"Computer_Points are {Computer_Points} and your points are {Human_Points} \n")
+      if game == _random:
+         print("That's a tie! 0 points to each \n")
 
-    elif _input == "r" and _random == "s":
-        Human_Points = Human_Points + 1
-        print(f"your guess is {_input} and computer guess is {_random} \n")
-        print("Human wins 1 point \n")
-        print(f"Computer_Points are {Computer_Points} and your points are {Human_Points} \n ")
+    #If user enters r
+      elif game == "r" and _random == "p":
+           Computer_Points = Computer_Points + 1
+           print(f"Your guess is {game} and Computer's guess is {_random} \n")
+           print("Computer wins 1 point \n")
+           print(f"Computer's Points are {Computer_Points} and your points are {Human_Points} \n")
+
+      elif game == "r" and _random == "s":
+           Human_Points = Human_Points + 1
+           print(f"Your guess is {game} and Computer's guess is {_random} \n")
+           print("Human wins 1 point \n")
+           print(f"Computer's Points are {Computer_Points} and your points are {Human_Points} \n ")
 
     # If user enters p
-    elif _input == "p" and _random == "s":
-        Computer_Points = Computer_Points + 1
-        print(f"your guess is {_input} and computer guess is {_random} \n")
-        print("Human wins 1 point \n")
-        print(f"Computer_Points are {Computer_Points} and your points are {Human_Points} \n")
+      elif game == "p" and _random == "s":
+           Computer_Points = Computer_Points + 1
+           print(f"Your guess is {game} and Computer's guess is {_random} \n")
+           print("Human wins 1 point \n")
+           print(f"Computer's Points are {Computer_Points} and your points are {Human_Points} \n")
 
-    elif _input == "p" and _random == "r":
-        Human_Points = Human_Points + 1
-        print(f"your guess is {_input} and computer guess is {_random} \n")
-        print("Human wins 1 point \n")
-        print(f"Computer_Points are {Computer_Points} and your points are {Human_Points} \n")
+      elif game == "p" and random == "r":
+           Human_Points = Human_Points + 1
+           print(f"Your guess is {game} and Computer's guess is {_random} \n")
+           print("Human wins 1 point \n")
+           print(f"Computer's Points are {Computer_Points} and your points are {Human_Points} \n")
 
     # If user enters s
-    elif _input == "s" and random == "r":
-        Computer_Points = Computer_Points + 1
-        print(f"Your guess is {_input} and computer guess is {_random} \n")
-        print("Computer wins 1 point \n")
-        print(f"Computer_Points are {Computer_Points} and your points are {Human_Points} \n ")
+      elif game == "s" and _random == "r":
+           Computer_Points = Computer_Points + 1
+           print(f"Your guess is {game} and computer's guess is {_random} \n")
+           print("Computer wins 1 point \n")
+           print(f"Computer's Points are {Computer_Points} and your points are {Human_Points} \n ")
 
-    elif _input == "s" and random == "p":
-        Human_Points = Human_Points + 1
-        print(f"Your guess is {_input} and computer guess is {_random} \n")
-        print("Computer wins 1 point \n")
-        print(f"Computer_Points are {Computer_Points} and your points are {Human_Points} \n ")
+      elif game == "s" and _random == "p":
+           Human_Points = Human_Points + 1
+           print(f"Your guess is {game} and computer's guess is {_random} \n")
+           print("Computer wins 1 point \n")
+           print(f"Computer's Points are {Computer_Points} and your points are {Human_Points} \n ")
 
-    else:
-        print("you have given wrong input \n")
+      else:
+           print("You have given wrong input.\n")
 
-    No_Of_Chances = No_Of_Chances + 1
-    print(f"{Total_Chances - No_Of_Chances} is left out of {Total_Chances} \n")
+      No_Of_Chances = No_Of_Chances + 1
+      print(f"{Total_Chances - No_Of_Chances} is left out of {Total_Chances} \n")
 
 print("Game over")
 
 if Computer_Points == Human_Points:
-    print("Tie")
+   print("Tie!")
 
 elif Computer_Points > Human_Points:
-    print("Computer wins and you lost")
+     print("Computer wins and you lost!")
 
 else:
-    print("You win and computer lost")
+    print("You won and computer lost!")
 
 print(f"Your points are {Human_Points} and computer's points are {Computer_Points}")
+# Rock Paper Scissors Game in Python
+# The rock hits scissors, paper wraps the rock, and scissors cuts paper.
 
